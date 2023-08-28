0c1b256c0cb33625cbcaca988bdbfc70e9e09896
Axel Sirota <axel.sirota@gmail.com>
2023-08-28 01:47:45 -0300

    demo1 module 5

---
+++ module5/demo1/src/main/java/com/globomantics/Main.java
@@ -0,0 +1,54 @@
+package com.globomantics;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.common.serialization.DoubleSerializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Properties;
+
+public class Main {
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+	private static final String TOPIC = "myorders";
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Properties props = new Properties();
+		props.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "http://localhost:9092,http://localhost:9093,http://localhost:9094");
+		props.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+		props.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, DoubleSerializer.class.getName());
+		KafkaProducer<String, Double> producer = new KafkaProducer<>(props);
+		String stateString =
+				"AK,AL,AZ,AR,CA,CO,CT,DE,FL,GA," +
+						"HI,ID,IL,IN,IA,KS,KY,LA,ME,MD," +
+						"MA,MI,MN,MS,MO,MT,NE,NV,NH,NJ," +
+						"NM,NY,NC,ND,OH,OK,OR,PA,RI,SC," +
+						"SD,TN,TX,UT,VT,VA,WA,WV,WI,WY";
+		String[] stateArray = stateString.split(",");
+		for (int i = 0; i < 25000; i++) {
+			String key = stateArray[(int) Math.floor(Math.random()*(50))];
+			double value = Math.floor(Math.random()* (10000-10+1)+10);
+			ProducerRecord<String, Double> producerRecord =
+					new ProducerRecord<>(TOPIC, key, value);
+
+			log.info("Sending message with key " + key + " to Kafka");
+
+			producer.send(producerRecord, (metadata, e) -> {
+				if (metadata != null) {
+					System.out.println(producerRecord.key());
+					System.out.println(producerRecord.value());
+					System.out.println(metadata.toString());
+				}
+			});
+			Thread.sleep(5000);
+		}
+		producer.flush();
+		producer.close();
+
+		log.info("Successfully produced messages to " + TOPIC + " topic");
+
+	}
+}
